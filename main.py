251e09f62daae62137618d4829439a8e496331c0
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-11-05 11:17:13 -0500

    Solution exercise 2

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-#Eexercise 1
-Voltage1 =float(input("""In this program i'll request to you insert the '5 Voltage's' and i'll saw you the average, and say you if is 'High voltage'}
-Please enter the first voltage: """))
-Voltage2 =float(input("Please enter the second voltage: "))
-Voltage3 =float(input("Please enter the third voltage: "))
-Voltage4 =float(input("Please enter the fourth voltage: "))
-Voltage5 =float(input("Please enter the fifth voltage: "))
-Average = ((Voltage1+Voltage2+Voltage3+Voltage4+Voltage5)/5)
-if average >=220:
-    print("High voltage")
-    else:
-        print("Correct voltage")
+#1. Desarrolle el código fuente de un programa que permita calcular el área de un triángulo equilátero, adicional visualizar `"DATOS NO VÁLIDOS"`, si el área es mayor a 1000.
+#
+#  La fórmula para calcular el área `A` de un triángulo equilátero de lado `a` es:
+#
+#
+#   ![](https://i.ibb.co/PTy9hV9/image.png)
+#A=((Raiz de 3)/4)xaElevado a la 2
+#   **Explicación**:
+#
+#   - Un triángulo equilátero tiene todos sus lados iguales, y sus ángulos interiores son todos de 60 grados.
+#   - Esta fórmula se deriva usando trigonometría y geometría básica aplicadas a un triángulo equilátero.
+import math
+side = float(input("""In this program i'l calculate the area of a equilateral triangle
+Please enter the side of any side of the triangle: """))
+area = math.triangle(side)
+unvalidData =0
+if area > 1000:
+    print("unvalid Data")
+else:
+    print(f"The area of the triangle is: {area}")
