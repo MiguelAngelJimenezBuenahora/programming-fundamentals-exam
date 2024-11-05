3b354dacecaae0f774c473375c9ea30368a67024
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-11-05 11:29:19 -0500

    Solution exercise 4

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-#Desarrolle un programa que solicite ingrese tres voltajes distintos e indique si el promedio de los voltajes ingresados es menor a 115 visualice
-# `"VOLTAJE CORRECTO"`, caso contrario sea mayor a 115 y menor a 220 visualice `"ALTO VOLTAJE"`, y si es mayor a 220 visualice `"PELIGRO"`.
-
-Voltage1 =int(input("""In this program i'l calculate the average voltage and say you if the voltage is the correct voltage, high voltage or danger
-Please enter the first voltage: """)) 
-Voltage2 =int(input("Please enter the second voltage: "))
-Voltage3 =int(input("Please enter the third voltage: "))
-average = ((Voltage1 + Voltage2 + Voltage3)/3)
-if average >=220:
-    Print("Danger")
-    elif average >=115 and average<220:
-        Print("High voltage")
-    else:
-        Print("Correct voltage")
+#4. Desarrolle el código fuente de un programa que permita ingresar y leer el valor correspondiente a una distancia en metros y la visualice expresadas en km.
+#
+#   Para convertir `metros` a `kilómetros`, puedes usar la siguiente fórmula: KM = m/1000
+meters = float(input(""""In this program , you enter yous length on metters and i'll convert that length to km 
+Please enter the length you want to convert: """))
+print(f"""The length on km is {round((meters/1000),2)}""")
